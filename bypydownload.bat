bb1e1cabc5817fadb044311726411a86e55e2895
Jack-XHP <haoping.xu@mail.utoronto.ca>
2022-04-20 22:19:54 -0400

    include bat files

---
+++ bypydownload.bat
@@ -0,0 +1,2 @@
+cd C:\Users\HomeNas\PycharmProjects\BaiduPanFilesTransfers\
+python .\bypyDownloader.py
---
+++ fanxinzhuirss.bat
@@ -0,0 +1,2 @@
+cd C:\Users\HomeNas\PycharmProjects\BaiduPanFilesTransfers\
+python .\rssfeed.py
